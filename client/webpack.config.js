6af41a4da43b48767dbea0b7317387ba66e737eb
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-04 21:38:44 -0600

    added css loader to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -50,7 +50,10 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
       ],
     },
   };
