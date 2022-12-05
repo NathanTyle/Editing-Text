e5dbb6da683ed2f97cd3107779c058a2b1bc7a9b
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-12-04 21:30:44 -0600

    added mainfest file to workbox plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,25 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
-      
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "JATE",
+        shortname: "JATE",
+        description: "Just another text editor",
+        background_color: "#272822",
+        theme_color: "#272822",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons",)
+          },
+        ],
+      }),
     ],
 
     module: {
